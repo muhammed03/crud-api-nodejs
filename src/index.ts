331bbabcdf4a3c22eb36535e91ea607213f76648
Muhammed Abdrahman <muhammed03amin.com>
2022-06-20 02:42:20 +0600

    feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+import dotenv from 'dotenv';
+import http from 'http';
+
+import controller from './controller'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+export const server = http.createServer((req, res) => {
+  controller(req, res);
+});
+
+server.listen(+PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
